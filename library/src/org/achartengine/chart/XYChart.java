7430eae2f07c53cabf20bb9c853c95b9c9586917
Brais Gabin <brais.gabin@inqbarna.com>
2013-02-08 17:41:52 +0100

    Fix Overlaping with titleX

--- library/src/org/achartengine/chart/XYChart.java
+++ library/src/org/achartengine/chart/XYChart.java
@@ -400,7 +400,7 @@ public abstract class XYChart extends AbstractChart {
         paint.setTextAlign(Align.CENTER);
         if (or == Orientation.HORIZONTAL) {
           drawText(canvas, mRenderer.getXTitle(), x + width / 2,
-              bottom + mRenderer.getLabelsTextSize() * 4 / 3 + size, paint, 0);
+              bottom + mRenderer.getLabelsTextSize() * 4 / 3 + mRenderer.getXLabelsPadding() + size, paint, 0);
           for (int i = 0; i < maxScaleNumber; i++) {
             Align axisAlign = mRenderer.getYAxisAlign(i);
             if (axisAlign == Align.LEFT) {
@@ -413,7 +413,7 @@ public abstract class XYChart extends AbstractChart {
           drawText(canvas, mRenderer.getChartTitle(), x + width / 2,
               y + mRenderer.getChartTitleTextSize(), paint, 0);
         } else if (or == Orientation.VERTICAL) {
-          drawText(canvas, mRenderer.getXTitle(), x + width / 2, y + height - size, paint, -90);
+          drawText(canvas, mRenderer.getXTitle(), x + width / 2, y + height - size + mRenderer.getXLabelsPadding() , paint, -90);
           drawText(canvas, mRenderer.getYTitle(), right + 20, y + height / 2, paint, 0);
           paint.setTextSize(mRenderer.getChartTitleTextSize());
           drawText(canvas, mRenderer.getChartTitle(), x + size, top + height / 2, paint, 0);
